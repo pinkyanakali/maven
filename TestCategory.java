8b5d7347e32bfa136668add22962ecadf3489631
pinkyanakali <bharathianakali@gmail.com>
2016-08-04 20:51:49 +0530

    Add files via upload

---
+++ TestCategory.java
@@ -0,0 +1,15 @@
+package com.niit.shoppingcart;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestCategory {
+	public static void main(String[] args){
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext();
+		context.scan("com.niit");
+		context.refresh();
+		context.getBean("category");
+		System.out.println("Bean created successfully");
+	}
+
+}
+
